9328c5d57955ff20b127d15126def25ea4cfed02
lauratiihonen <lauraomnia@gmail.com>
2021-11-15 18:11:47 +0200

    1st release

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 Web application where you can plan journeys for VR train services.
 Might be too challenging for me but we will see lol
 
-https://www.digitraffic.fi/rautatieliikenne/
+--
+15/11/2021
 
+- User can plan trips between Tampere and Helsinki
+- User can see journey date, departure time and arrival time
+- in progress - trying to fetch station shortnames and full names from different api and adding them to dropdowns menu options
+
+https://www.digitraffic.fi/rautatieliikenne/
+https://rata.digitraffic.fi/api/v1/metadata/stations
+https://rata.digitraffic.fi/api/v1/live-trains/station/HKI/TPE}
 https://rata.digitraffic.fi/api/v1/live-trains/station/HKI?arrived_trains=5&arriving_trains=5&departed_trains=5&departing_trains=5&include_nonstopping=false&train_categories=Commuter
--- src/App.js
+++ src/App.js
@@ -1,37 +1,66 @@
 import "./App.css";
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import cities from "./cities.json";
 import Select from "react-select";
+
 const city = [
   { value: "HKI", label: "Helsinki" },
   { value: "TPE", label: "Tampere" },
 ];
 
 function App() {
+  //const [fetchCity, setCity] = useState("");
   const [departure, setDeparture] = useState("");
   const [destination, setDestination] = useState("");
   const [date, setDate] = useState("");
   const [departureTime, setDepartureTime] = useState("");
   const [destinationTime, setDestinationTime] = useState("");
 
-  async function fetchPerson() {
+  /*async function fetchCityNames() {
+    let hr = await fetch(
+      `https://rata.digitraffic.fi/api/v1/metadata/stations`
+    );
+    let cityNames = await hr.json();
+
+    for (var i = 0; cityNames.length(); i++)
+      if (cityNames[i].passengerTraffic === "true") {
+        cities = [
+          {
+            value: `${cityNames[0].stationShortCode}`,
+            label: `${cityNames[0].stationName}`,
+          },
+        ];
+      }
+  }*/
+
+  async function fetchJourney() {
     let hr = await fetch(
       `https://rata.digitraffic.fi/api/v1/live-trains/station/${departure}/${destination}`
     );
     let data = await hr.json();
-    console.log(data);
+
     setDate(data[0].departureDate);
-    setDepartureTime(data[0].timeTableRows[0].scheduledTime);
-    setDestinationTime(data[0].timeTableRows[89].scheduledTime);
+    for (var i = 0; i < Object.keys(data[0].timeTableRows).length; i++) {
+      if (
+        data[0].timeTableRows[i].stationShortCode === departure &&
+        data[0].timeTableRows[i].type === "DEPARTURE"
+      ) {
+        setDepartureTime(data[0].timeTableRows[i].scheduledTime);
+      }
+      if (
+        data[0].timeTableRows[i].stationShortCode === destination &&
+        data[0].timeTableRows[i].type === "ARRIVAL"
+      ) {
+        setDestinationTime(data[0].timeTableRows[i].scheduledTime);
+      }
+    }
   }
 
   return (
     <div className="App">
       <h1>Journey planner</h1>
       <Select
-        id="departureSelect"
         options={city}
         className="mb-3"
         placeholder="Departure"
@@ -39,14 +68,13 @@ function App() {
         onChange={(event) => setDeparture(event.value)}
       />
       <Select
-        id="destinationSelect"
         options={city}
         className="mb-3"
         placeholder="Destination"
         isSearchable
         onChange={(event) => setDestination(event.value)}
       />
-      <button onClick={fetchPerson}>Get Journey</button>
+      <button onClick={fetchJourney}>Get Journey</button>
       <p>Journey date: {date}</p>
       <p>Departure time: {departureTime}</p>
       <p>Arrival: {destinationTime}</p>
